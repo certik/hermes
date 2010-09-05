9eca84d3ba6a5ec4b97fed6f3b0e1b7a3a36639a
Ondrej Certik <ondrej@certik.cz>
2010-09-04 19:21:50 -0700

    GAMM channel example added
    This example implements GAMM channel using Euler equations without any
    stabilization or DG to show that it blows up.

---
+++ hermes2d/examples/gamm-channel/forms.h
@@ -0,0 +1,21 @@
+#ifndef __HERMES2D_SEA_BREEZE_FORMS_H
+#define __HERMES2D_SEA_BREEZE_FORMS_H
+
+#include "hermes2d.h"
+#include "params.h"
+
+//  boundary markers
+#define marker_bottom 1
+#define marker_right 2
+#define marker_top 3
+#define marker_left 4
+
+void set_iteration(int i);
+void register_bc(Space &s0, Space &s1, Space &s3, Space &s4);
+void set_ic(Mesh &mesh, Solution &w0, Solution &w1, Solution &w3, Solution &w4);
+void register_forms(WeakForm &wf, Solution &w0_prev, Solution &w1_prev,
+        Solution &w3_prev, Solution &w4_prev);
+
+extern const double TAU;
+
+#endif
